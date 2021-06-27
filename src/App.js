1a7ff4ec13d00327a98c6400eaf28c98b54a5e68
Gal <galpalas89@gmail.com>
2021-06-27 17:18:26 +0300

    create navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import WelcomePage from './pages/welcomePage';
 
 function App() {
   return (
     <div className="App">
-     
+     <WelcomePage />
     </div>
   );
 }
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./navbar.css";
+
+function Navbar() {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-md bg-dark navbar-dark py-3">
+                <div className="container">
+                    <a href="/#" className="navbar-brand header">Netflix</a>
+                
+
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu"><span className="navbar-toggler-icon"></span></button>
+
+                <div className="collapse navbar-collapse" id="navmenu">
+                    <ul className="nav navbar-nav ms-auto">
+                        <li className="nav-item">
+                            <a href="#about" className="nav-link">About</a>
+                        </li>
+                    </ul>
+                </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
