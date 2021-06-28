14bb2374b6e525163eecbf5ee2a97dd2cb285ecc
Gal <galpalas89@gmail.com>
2021-06-28 12:58:18 +0300

    create showcase component

--- src/pages/welcomePage.jsx
+++ src/pages/welcomePage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Navbar from '../components/navbar/navbar';
+import Showcase from '../components/showcase/showcase';
 function WelcomePage() {
     return (
         <div>
             <Navbar/>
+            <Showcase/>
         </div>
     )
 }
