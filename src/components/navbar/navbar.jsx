cfcdd8b20bf62cedcdb6d3aec8e965558c402238
Gal <galpalas89@gmail.com>
2021-06-29 22:47:03 +0300

    create footer section

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,27 +1,42 @@
-import React from 'react'
+import React from "react";
 import "./navbar.css";
 
 function Navbar() {
-    return (
-        <div>
-            <nav className="navbar navbar-expand-md bg-dark navbar-dark py-3">
-                <div className="container">
-                    <a href="/#" className="navbar-brand header">Netflix</a>
-                
+  return (
+    <div>
+      <nav className="navbar navbar-expand-md bg-dark navbar-dark py-3">
+        <div className="container">
+          <a href="/#" className="navbar-brand header">
+            Netflix
+          </a>
 
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu"><span className="navbar-toggler-icon"></span></button>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navmenu"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-                <div className="collapse navbar-collapse" id="navmenu">
-                    <ul className="nav navbar-nav ms-auto">
-                        <li className="nav-item">
-                            <a href="#about" className="nav-link">About</a>
-                        </li>
-                    </ul>
-                </div>
-                </div>
-            </nav>
+          <div className="collapse navbar-collapse" id="navmenu">
+            <ul className="nav navbar-nav ms-auto">
+              <li className="nav-item">
+                <a href="#questions" className="nav-link">
+                  Questions
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#contact" className="nav-link">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-    )
+      </nav>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
